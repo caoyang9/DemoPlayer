c3238cbffa8a6a73d285bce034bb0ca59a2075c5
caoyang3 <1791200683@qq.com>
2026-03-06 15:32:25 +0800

    feat: 暗码卸载演示应用扩展。

--- app/src/main/java/com/android/retaildemo/DialerCodeReceiver.java
+++ app/src/main/java/com/android/retaildemo/DialerCodeReceiver.java
@@ -13,19 +13,34 @@ import android.util.Log;
 public class DialerCodeReceiver extends BroadcastReceiver {
     private static final String TAG = "DialerCodeReceiver";
     private static final String DIALER_CODE_ACTION = "android.provider.Telephony.SECRET_CODE";
-    private static final String DialerCode = "9876";
+    private static final String StartDialerCode = "9876";
+    private static final String UninstallDialerCode = "5010";
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive()");
         if (intent != null && DIALER_CODE_ACTION.equals(intent.getAction())) {
             Uri uri = intent.getData();
-            if (uri != null && uri.getHost() != null && uri.getHost().equals(DialerCode)){
+            if (uri != null && uri.getHost() != null && uri.getHost().equals(StartDialerCode)){
                 Log.d(TAG, "收到暗码广播：" + uri.getHost() + "，启动RetailDemo");
                 // 启动 DemoPlayer
                 Intent i = new Intent(context, DemoPlayer.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
             }
+            if (uri != null && uri.getHost() != null && uri.getHost().equals(UninstallDialerCode)){
+                // 准备卸载 DemoPlayer
+                showUninstallDialog(context);
+            }
         }
     }
+
+    /**
+     * 卸载演示应用：弹出提示框，请求卸载确认
+     * @param context
+     */
+    private void showUninstallDialog(Context context) {
+        // TODO 执行卸载
+        Log.d(TAG, "弹出卸载请求确认弹框");
+    }
+
 }
